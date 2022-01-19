691bb7bd12f2fa9c7d068ab7e32d5c01cdab4ff5
JunHwan Kim <nefertiri972@gmail.com>
2022-01-19 20:20:04 +0900

    Sorting, Searching 03. 삽입정렬

---
+++ src/main/java/problem/sorting_searching/problem03/Main.java
@@ -0,0 +1,51 @@
+package problem.sorting_searching.problem03;
+
+import java.util.Scanner;
+
+/**
+ * 3. 삽입 정렬
+ * 설명
+ * N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+ * 정렬하는 방법은 삽입정렬입니다.
+ *
+ * 입력
+ * 첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+ * 두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다. 각 자연수는 정수형 범위 안에 있습니다.
+ *
+ * 출력
+ * 오름차순으로 정렬된 수열을 출력합니다.
+ */
+
+public class Main {
+
+    public int[] solution(int n, int[] arr) {
+
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > temp) {
+                    arr[j + 1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            arr[j + 1] = temp;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args){
+        Main main = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int i : main.solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+}
